78d5878522805eca1067394f05d3fdf1912232b9
Jamshid <jroid8@tutanota.com>
2023-12-16 11:01:23 +0330

    formatting

--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,7 @@ const upload = multer({ storage: storage });
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
 
-app.use("/fileshare",express.static(path.join(__dirname, "..", "public")));
+app.use("/fileshare", express.static(path.join(__dirname, "..", "public")));
 
 app.post("/fileshare/upload", upload.array("files"), (_, res) => {
   res.send("uploaded");
@@ -38,19 +38,21 @@ app.post("/fileshare/upload", upload.array("files"), (_, res) => {
 
 app.all("*", (req, res) => {
   const req_path = path.join(share_dir, decodeURIComponent(req.path));
-	const show_hidden = req.query.h === "1";
+  const show_hidden = req.query.h === "1";
   if (!fs.existsSync(req_path)) res.sendStatus(404);
   else if (fs.lstatSync(req_path).isDirectory()) {
     res.render("index.ejs", {
-			show_hidden: show_hidden,
+      show_hidden: show_hidden,
       title: path.basename(req_path),
       directory: fs
         .readdirSync(req_path)
-				.filter((p) => show_hidden || !p.startsWith("."))
+        .filter((p) => show_hidden || !p.startsWith("."))
         .map((p) => {
           return {
             isDir: fs.lstatSync(path.join(req_path, p)).isDirectory(),
-            path: encodeURIComponent(path.join(req_path, p)) + (show_hidden ? "?h=1" : ""),
+            path:
+              encodeURIComponent(path.join(req_path, p)) +
+              (show_hidden ? "?h=1" : ""),
             name: p,
           };
         })
